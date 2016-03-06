1ad0b237df7b8706c92a56a5f6a7ff02f82d7ca1
Eric Londaits <eric.londaits@gmail.com>
2016-03-06 14:25:50 -0300

    Test environment setup adds editor role to test user

--- test_env_setup.js
+++ test_env_setup.js
@@ -2,15 +2,29 @@ var config = require('./tests/config');
 var exec = require('child_process').exec;
 var console = require('console');
 
-var command = "drush --root=\"" + config.localDrupalRootPath + "\" " +
+var userCreateCommand = "drush --root=\"" + config.localDrupalRootPath + "\" " +
     "user-create \"" + config.testUserName + "\" " +
     "--mail=\"" + config.testUserEmail + "\" " +
     "--password=\"" + config.testUserPass + "\" ";
 
-var child = exec(command, function (error, stdout, stderr) {
+var addRoleCommand = "drush --root=\"" + config.localDrupalRootPath + "\" " +
+    "user-add-role editor \"" + config.testUserName + "\" ";
+
+console.log("Creating user account " + config.testUserEmail);
+var child = exec(userCreateCommand, function (error, stdout, stderr) {
   console.log('stdout: ' + stdout);
   console.log('stderr: ' + stderr);
   if (error !== null) {
     console.log('exec error: ' + error);
   }
+
+  console.log("Adding editor role");
+  child = exec(addRoleCommand, function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+
 });
